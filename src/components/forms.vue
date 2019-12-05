fee261381cb0608bc14918402b4c6cf2bdfb8d80
YiFFFo <1359641169@qq.com>
2019-12-05 11:06:52 +0800

    update

--- src/components/forms.vue
+++ src/components/forms.vue
@@ -9,24 +9,44 @@
 
 		<form @submit="formSubmit">
 			<div class="name wrap">
-				<p>真实姓名: <i>*</i></p>
-				<input type="text" placeholder="2-4个中文字符" v-model="name" :class="nameFlag ? 'borderRed' :''" />
+				<p>
+					真实姓名:
+					<i>*</i>
+				</p>
+				<input
+					type="text"
+					placeholder="2-4个中文字符"
+					@input="check1"
+					maxlength="4"
+					focus="true"
+					v-model="name"
+					:class="nameFlag ? 'borderRed' :''"
+				/>
 			</div>
 			<div class="tel wrap">
-				<p>电<span>话:</span><i>*</i></p>
+				<p>
+					电
+					<span>话:</span>
+					<i>*</i>
+				</p>
 				<input
 					type="text"
 					placeholder="11位手机号:1xx-xxx-xxxx"
+					@input="check2"
+					maxlength="11"
 					v-model="tel"
 					:class="telFlag ? 'borderRed' :''"
 				/>
 			</div>
-			
+
 			<div class="major wrap">
-				<p>意向专业:<i>*</i></p>
+				<p>
+					意向专业:
+					<i>*</i>
+				</p>
 				<input type="text" placeholder="例如:航空专业" v-model="major" />
 			</div>
-            <div class="wx wrap">
+			<div class="wx wrap">
 				<p>微信/QQ:</p>
 				<input type="text" placeholder="输入微信或QQ" v-model="wx" />
 			</div>
@@ -53,6 +73,8 @@
 	</div>
 </template>
 <script>
+let nameReg = new RegExp(/^[\u4E00-\u9FA5]{2,4}$/);
+let telReg = new RegExp(/^1[3456789]\d{9}$/);
 export default {
 	data() {
 		return {
@@ -63,22 +85,21 @@ export default {
 			arrIndex: 0,
 			selector: ["高中", "初中", "其他"],
 
-			nameFlag: false,
-			telFlag: false
+			nameFlag: true,
+			telFlag: true
 		};
 	},
 	methods: {
 		formSubmit(e) {
-			const that = this;
-			let nameReg = new RegExp(/^[\u4E00-\u9FA5]{2,4}$/);
-			let telReg = new RegExp(/^1[3456789]\d{9}$/);
-
-			if (!telReg.test(this.tel)) this.telFlag = true;
-			if (!nameReg.test(this.name)) this.nameFlag = true;
-			if (!telReg.test(this.tel) || !nameReg.test(this.name)) {
-				// console.log("");
+			if (this.nameFlag == false || !this.telFlag == false) {
 				mpvue.showToast({
 					title: "请检查姓名或者手机号格式是否正确",
+					icon: "none"
+				});
+			}
+			if (this.nameFlag == false && this.telFlag == false) {
+				mpvue.showToast({
+					title: "提交成功",
 					icon: "none"
 				});
 			}
@@ -88,6 +109,21 @@ export default {
 		},
 		cancel(e) {
 			console.log(e);
+		},
+		check(reg, value) {
+			if (reg.test(value)) {
+				return false;
+			} else {
+				return true;
+			}
+		},
+		check1(e) {
+			let value = e.mp.detail.value;
+			this.nameFlag = this.check(nameReg, value);
+		},
+		check2(e) {
+			let value = e.mp.detail.value;
+			this.telFlag = this.check(telReg, value);
 		}
 	}
 };
@@ -103,17 +139,17 @@ form {
 		margin-bottom: 20rpx;
 
 		p {
-            width: 140rpx;
-            position: relative;
+			width: 140rpx;
+			position: relative;
 			span {
 				margin-left: 55rpx;
-            }
-            i{
-                color: red;
-                position: absolute;
-                top: 0;
-                left:-20rpx;
-            }
+			}
+			i {
+				color: red;
+				position: absolute;
+				top: 0;
+				left: -20rpx;
+			}
 		}
 		input::placeholder {
 			font-size: 12px;
---
+++ src/pages/zxbm/index.vue
@@ -0,0 +1,306 @@
+<template>
+	<div>
+		<headF></headF>
+		<div class="now-pos padding">
+			<!-- <img src="" alt=""> -->
+			<span>当前位置：首页>院校选择</span>
+		</div>
+
+		<bannerSwiper></bannerSwiper>
+
+		
+
+        <forms></forms>
+		<footerF></footerF>
+	</div>
+</template>
+
+<script>
+import headF from "@/components/headF";
+import footerF from "@/components/footerF";
+import bannerSwiper from "@/components/bannerSwiper";
+import forms from "@/components/forms";
+
+export default {
+	components: {
+		headF,
+		footerF,
+        bannerSwiper,
+        forms
+	},
+	data() {
+		return {
+			//分类数据
+			headList: ["类别", "地区", "等级"],
+			classifyArray: [
+				[
+					{
+						text: "类别不限",
+						url: ""
+					},
+					{
+						text: "中专院校",
+						url: ""
+					},
+					{
+						text: "技工院校",
+						url: ""
+					},
+					{
+						text: "职业高中",
+						url: ""
+					},
+					{
+						text: "普通高中",
+						url: ""
+					},
+					{
+						text: "五年制大专",
+						url: ""
+					}
+				],
+				[
+					{
+						text: "全部",
+						url: ""
+					},
+					{
+						text: "四川",
+						url: "",
+						children: [
+							{
+								text: "全部",
+								url: ""
+							},
+							{
+								text: "成都市",
+								url: ""
+							},
+							{
+								text: "自贡市",
+								url: ""
+							},
+							{
+								text: "攀枝花市",
+								url: ""
+							},
+							{
+								text: "泸州市",
+								url: ""
+							},
+							{
+								text: "成都市",
+								url: ""
+							},
+							{
+								text: "自贡市",
+								url: ""
+							},
+							{
+								text: "攀枝花市",
+								url: ""
+							},
+							{
+								text: "泸州市",
+								url: ""
+							},
+							{
+								text: "成都市",
+								url: ""
+							},
+							{
+								text: "自贡市",
+								url: ""
+							},
+							{
+								text: "攀枝花市",
+								url: ""
+							},
+							{
+								text: "泸州市",
+								url: ""
+							}
+						]
+					},
+					{
+						text: "贵州",
+						url: "",
+						children: [
+							{
+								text: "全部",
+								url: ""
+							},
+							{
+								text: "贵阳市",
+								url: ""
+							},
+							{
+								text: "六盘水市",
+								url: ""
+							},
+							{
+								text: "遵义市",
+								url: ""
+							},
+							{
+								text: "安顺市",
+								url: ""
+							}
+						]
+					}
+				],
+				[
+					{
+						text: "等级不限",
+						url: ""
+					},
+					{
+						text: "国家级示范",
+						url: ""
+					},
+					{
+						text: "国家级重点",
+						url: ""
+					},
+					{
+						text: "省级示范",
+						url: ""
+					},
+					{
+						text: "省级重点",
+						url: ""
+					},
+					{
+						text: "普通院校",
+						url: ""
+					},
+					{
+						text: "市级示范",
+						url: ""
+					}
+				]
+			],
+			classifyIndex: 0,
+			//专业列表数据
+			schList: [
+				{
+					title: "学校名称学校名称学校名称学校名称",
+					nature: ["中专院校", "国家级重点"],
+                    address: "学校地址学校地址",
+                    hotMajor:["电子商务","美术设计与制作"]
+				},
+				{
+					title: "学校名称学校名称学校名称学校名称",
+					nature: ["中专院校", "国家级重点"],
+                    address: "学校地址学校地址",
+                    hotMajor:["电子商务","美术设计与制作"]
+				},
+				{
+					title: "学校名称学校名称学校名称学校名称",
+					nature: ["中专院校", "国家级重点"],
+                    address: "学校地址学校地址",
+                    hotMajor:["电子商务","美术设计与制作"]
+				},
+				{
+					title: "学校名称学校名称学校名称学校名称",
+					nature: ["中专院校", "国家级重点"],
+                    address: "学校地址学校地址",
+                    hotMajor:["电子商务","美术设计与制作"]
+				}
+			],
+		};
+	},
+
+	created() {
+		let app = getApp();
+	},
+	mounted() {},
+	methods: {
+		getSelectClassifyIndex(index) {
+			this.classifyIndex = index;
+        },
+        getData(data){
+            this.schList = data
+        }
+	}
+};
+</script>
+
+<style lang="less" scope>
+.body-tab {
+    &>div:last-of-type{
+        border-bottom: none;
+    }
+	.tab-list {
+		padding-top: 30rpx;
+		padding-bottom: 10rpx;
+        border-bottom: 1rpx solid #f5f5f5;
+		.list-a {
+			display: flex;
+			.img {
+				width: 200rpx;
+				height: 150rpx;
+				background-color: #ccc;
+				flex-shrink: 0;
+				border-radius: 10rpx;
+				overflow: hidden;
+
+				img {
+					width: 100%;
+				}
+			}
+			.con {
+				flex: 1;
+				margin-left: 20rpx;
+				min-width: 0;
+				h4 {
+					font-size: 16px;
+				}
+				.sch-xz {
+					// background-color: #ccc;
+					margin: 10rpx 0;
+					span {
+						border-radius: 40rpx;
+						margin-right: 10rpx;
+						font-size: 12px;
+					}
+					.xz_1 {
+						padding: 0 10rpx;
+						border: 1rpx solid #ed5d2b;
+						color: #ed5d2b;
+					}
+					.xz_2 {
+						padding: 0 10rpx;
+						border: 1rpx solid #00b2fc;
+						color: #00b2fc;
+					}
+				}
+
+				.hot-zy {
+					font-size: 12px;
+					display: flex;
+					white-space: nowrap;
+					flex-wrap: wrap;
+					span {
+						margin-right: 10px;
+						color: #666;
+					}
+				}
+			}
+		}
+
+		.sch-adress {
+			width: fit-content;
+			padding: 4rpx 20rpx;
+			background-color: #f5f5f5;
+			border-radius: 40rpx;
+			margin-top: 10rpx;
+
+			span {
+				font-size: 12px;
+				color: #999;
+			}
+		}
+	}
+}
+</style>
